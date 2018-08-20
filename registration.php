b14f01f55707b57761d75b349470043019ee7934
Jignesh <rudani.jignesh@gmail.com>
2018-08-20 19:02:01 +0530

    PHP Code Sniffer Checking

--- registration.php
+++ registration.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Atharva Bulkadd module registration.
+ *
+ * PHP version 7.0
+ * 
+ * @category PHP
+ * @package  Atharva_Bulkadd
+ * @author   expert <expertdev9@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version  GIT: XXXXXXXXXXXXXXXXXX
+ * @link     https://www.google.co.in
+ */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Atharva_Bulkadd',
